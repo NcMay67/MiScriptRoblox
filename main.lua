3515a1f22480c94b017047956c7a1a624a52a45d
NcMay67 <ncmay69@gmail.com>
2026-08-11 21:24:19 +0200

    update main.lua

---
+++ main.lua
@@ -0,0 +1,38 @@
+-- ==========================================
+-- NC HUB | OFFICIAL LOADER
+-- ==========================================
+local PlaceId = game.PlaceId
+local BaseURL = "https://raw.githubusercontent.com/May67-dev/MiScriptRoblox/refs/heads/main/"
+
+-- Diccionario de Juegos (ID = Nombre del archivo en GitHub)
+local Games = {
+    [15197136141] = "FactoryTycoon.lua",
+    [131795157971706] = "TSL.lua",
+    [142823291]    = "MM2.lua",
+    [118055952211055] = "HackABusiness.lua",
+    [127634932179327] = "KickBall.lua",
+    [71785281157268] = "GemCrusher.lua"
+}
+
+-- Determinamos qué archivo cargar
+local scriptToLoad = Games[PlaceId] or "Universal.lua"
+
+-- Notificación en consola para depuración
+print("------------------------------------------")
+print("[NC HUB] Cargando sistema modular...")
+print("[NC HUB] Juego Detectado (ID): " .. PlaceId)
+print("[NC HUB] Archivo a cargar: " .. scriptToLoad)
+print("------------------------------------------")
+
+-- Ejecución del script
+local success, err = pcall(function()
+    loadstring(game:HttpGet(BaseURL .. scriptToLoad))()
+end)
+
+if not success then
+    warn("[NC HUB] Error crítico al cargar: " .. tostring(err))
+    -- Respaldo: Intentar cargar el Universal si el específico falla
+    pcall(function()
+        loadstring(game:HttpGet(BaseURL .. "Universal.lua"))()
+    end)
+end
