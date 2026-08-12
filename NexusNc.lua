2692cee73119baeb364b9706962bfadd68c36bb2
NcMay67 <ncmay69@gmail.com>
2026-08-12 23:29:35 +0200

    update NexusNc.lua

--- NexusNc.lua
+++ NexusNc.lua
@@ -1730,7 +1730,7 @@ function NX:Badge(card, config)
     })
     Util.Round(badge, 8)
     Util.Make("UIPadding", {
-        PaddingLeft = UDim.new(0, 14),
+        PaddingLeft = UDim.new(0, 8),
         PaddingRight = UDim.new(0, 9),
         Parent = badge
     })
